d528d18b2e62342d8dd736bac6d5eb19c267b348
Iris Berger <87372230+irisberger18@users.noreply.github.com>
2021-11-18 23:12:41 +0200

    script

--- HW2/hw2_script.py
+++ HW2/hw2_script.py
@@ -1,5 +1,6 @@
-from hw2_functions import *
 import os
+
+from hw2_functions import *
 
 if __name__ == "__main__":
     # # Read images from folder
@@ -33,35 +34,92 @@ if __name__ == "__main__":
     face_img2 = cv2.imread(path_image)
     face_img2_gray = cv2.cvtColor(face_img2, cv2.COLOR_BGR2GRAY)
 
-    path_image = r'C:\dev\Image-Processing-HW\HW2\FaceImages\Face3.tif'
+    path_image = os.path.join(dir_path, "Face3.tif")
     section_b_img1 = cv2.imread(path_image)
     section_b_img1_gray = cv2.cvtColor(section_b_img1, cv2.COLOR_BGR2GRAY)
 
-    path_image = r'C:\dev\Image-Processing-HW\HW2\FaceImages\Face4.tif'
+    path_image = os.path.join(dir_path, "Face4.tif")
     section_b_img2 = cv2.imread(path_image)
     section_b_img2_gray = cv2.cvtColor(section_b_img2, cv2.COLOR_BGR2GRAY)
 
-    getImagePts(face_img1, face_img2, "varName1", "varName2", 12)
+    getImagePts(face_img1, face_img2, "section_a1", "section_a2", 12)
+    getImagePts(face_img1, face_img2, "section_b1", "section_b2", 12)
+    getImagePts(face_img1, face_img2, "section_c1_1_small", "section_c2_1_small", 6)
+    getImagePts(face_img1, face_img2, "section_c1_1_large", "section_c2_1_large", 12)
+    getImagePts(face_img1, face_img2, "section_c1_2_distributed", "section_c2_2_distributed", 12)
+    getImagePts(face_img1, face_img2, "section_c1_2_focused", "section_c2_2_focused", 12)
 
     """section a"""
 
     # Load the 12 selected points
-    im1_pts = np.load("varName1.npy")
-    im2_pts = np.load("varName2.npy")
+    section_a1_pts = np.load("section_a1.npy")
+    section_a2_pts = np.load("section_a2.npy")
 
-    image_list = createMorphSequence(face_img1_gray, im1_pts, face_img2_gray, im2_pts, np.linspace(0, 1, 100), 1)
+    image_list = createMorphSequence(face_img1_gray, section_a1_pts, face_img2_gray, section_a2_pts,
+                                     np.linspace(0, 1, 100), 1)
     writeMorphingVideo(image_list, "transform_face_to_another")
 
-    # """section b"""
-    #
-    # # load point for the picture we choose
-    # im3_pts = np.load("varName3.npy")
-    # im4_pts = np.load("varName4.npy")
-    #
-    # # affine transformation
-    # affine_transform = findAffineTransform(im3_pts, im4_pts)
-    # mapImage(section_b_img1_gray, T, sizeOutIm)
-    #
-    # # projective transformation
-    # projective_transform = findProjectiveTransform(im3_pts, im4_pts)
-    # mapImage(section_b_img2_gray, T, sizeOutIm)
+    """section b"""
+
+    # load point for the picture we choose
+    section_b1_pts = np.load("section_b1.npy")
+    section_b2_pts = np.load("section_b2.npy")
+
+    # affine transformation
+    affine_transform = findAffineTransform(section_b1_pts, section_b2_pts)
+    affine_image = mapImage(section_b_img1_gray, affine_transform, section_b_img1_gray.shape)
+
+    # projective transformation
+    projective_transform = findProjectiveTransform(section_b1_pts, section_b2_pts)
+    projective_image = mapImage(section_b_img2_gray, projective_transform, section_b_img2_gray.shape)
+
+    # then display
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(affine_image, cmap='gray', vmin=0, vmax=255)
+    plt.title("Image after affine transformation")
+    plt.subplot(1, 2, 2)
+    plt.imshow(projective_image, cmap='gray', vmin=0, vmax=255)
+    plt.title("Image after projective transformation")
+
+    """section c"""
+
+    # display images with different amount of points - load point for the picture we choose
+    section_c1_1_small_pts = np.load("section_c1_1_small.npy")
+    section_c2_1_small_pts = np.load("section_c2_1_small.npy")
+    section_c1_1_large_pts = np.load("section_c1_1_large.npy")
+    section_c2_1_large_pts = np.load("section_c2_1_large.npy")
+
+    small_img = createMorphSequence(face_img1_gray, section_c1_1_small_pts, face_img2_gray, section_c2_1_small_pts,
+                                    np.linspace(0, 1, 2), 1)[-1]
+    large_img = createMorphSequence(face_img1_gray, section_c1_1_large_pts, face_img2_gray, section_c2_1_large_pts,
+                                    np.linspace(0, 1, 2), 1)[-1]
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(small_img, cmap='gray', vmin=0, vmax=255)
+    plt.title("Morph result with 6 points")
+    plt.subplot(1, 2, 2)
+    plt.imshow(large_img, cmap='gray', vmin=0, vmax=255)
+    plt.title("Morph result with 12 points")
+
+    # display images with different location of points - load point for the picture we choose
+    section_c1_2_distributed_pts = np.load("section_c1_2_distributed.npy")
+    section_c2_2_distributed_pts = np.load("section_c2_2_distributed.npy")
+    section_c1_2_focused_pts = np.load("section_c1_2_focused.npy")
+    section_c2_2_focused_pts = np.load("section_c2_2_focused.npy")
+
+    distributed_img = \
+        createMorphSequence(face_img1_gray, section_c1_2_distributed_pts, face_img2_gray, section_c2_2_distributed_pts,
+                            np.linspace(0, 1, 2), 1)[-1]
+    focused_img = \
+        createMorphSequence(face_img1_gray, section_c1_2_focused_pts, face_img2_gray, section_c2_2_focused_pts,
+                            np.linspace(0, 1, 2), 1)[-1]
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(small_img, cmap='gray', vmin=0, vmax=255)
+    plt.title("Morph result with distributed points")
+    plt.subplot(1, 2, 2)
+    plt.imshow(large_img, cmap='gray', vmin=0, vmax=255)
+    plt.title("Morph result with focused points")
