c65eeda1e479ebe502b8c2a7af1f5262d2ab533d
Shahar Tefler <shahar.tefler@neureality.ai>
2021-11-23 17:29:29 +0200

    Add Salt and pepper noise (I think)

--- HW3/hw3_functions.py
+++ HW3/hw3_functions.py
@@ -1,18 +1,30 @@
 import numpy as np
-import cv2
+import cv2.cv2 as cv2
 import random
 import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
 
+# Constants
+MAX_GRAY_VAL = 256
+
+
 def addSPnoise(im, p):
     sp_noise_im = im.copy()
-	# TODO: add implementation
+    # Generate random indexes for white and black pixels
+    white_pixels_x = random.sample(range(MAX_GRAY_VAL), int(p / 2))
+    white_pixels_y = random.sample(range(MAX_GRAY_VAL), int(p / 2))
+    black_pixels_x = random.sample(range(MAX_GRAY_VAL), int(p / 2))
+    black_pixels_y = random.sample(range(MAX_GRAY_VAL), int(p / 2))
+
+    # Assign white and black values
+    sp_noise_im[white_pixels_x, white_pixels_y] = MAX_GRAY_VAL
+    sp_noise_im[black_pixels_x, black_pixels_y] = 0
     return sp_noise_im
 
 
 def addGaussianNoise(im, s):
     gaussian_noise_im = im.copy()
-	# TODO: add implementation
+    # TODO: add implementation
     return gaussian_noise_im
 
 
@@ -23,7 +35,7 @@ def cleanImageMedian(im, radius):
 
 
 def cleanImageMean(im, radius, maskSTD):
-   	# TODO: add implementation
+    # TODO: add implementation
     return cleaned_im
 
 
@@ -31,5 +43,3 @@ def bilateralFilt(im, radius, stdSpatial, stdIntensity):
     bilateral_im = im.copy()
     # TODO: add implementation
     return bilateral_im
-
-
--- HW3/hw3_script.py
+++ HW3/hw3_script.py
@@ -1,6 +1,5 @@
 from hw3_functions import *
 
-
 if __name__ == "__main__":
     # feel free to load different image than lena
     lena = cv2.imread(r"Images\lena.tif")
@@ -8,9 +7,9 @@ if __name__ == "__main__":
 
     # 1 ----------------------------------------------------------
     # add salt and pepper noise - low
-    lena_sp_low = addSPnoise() # TODO - add low noise
-	
-	# add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
+    lena_sp_low = addSPnoise(lena_gray, 400)
+
+    # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
     plt.subplot(2, 3, 1)
     plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
@@ -18,87 +17,87 @@ if __name__ == "__main__":
     plt.subplot(2, 3, 2)
     plt.imshow(lena_sp_low, cmap='gray', vmin=0, vmax=255)
     plt.title("salt and pepper - low")
-    plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    plt.title("median")
-    plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    plt.title("mean")
-    plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
-    plt.title("bilateral")
-
-    print("Conclusions -----  TODO: add explanation\n")
-
-    # 2 ----------------------------------------------------------
-    # add salt and pepper noise - high
-    lena_sp_high = addSPnoise() # TODO - add low noise
-	
-	# add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
-    plt.figure()
-    plt.subplot(2, 3, 1)
-    plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
-    plt.title("original")
-    plt.subplot(2, 3, 2)
-    plt.imshow(lena_sp_high, cmap='gray', vmin=0, vmax=255)
-    plt.title("salt and pepper - low")
-    plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    plt.title("median")
-    plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    plt.title("mean")
-    plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
-    plt.title("bilateral")
-
-    print("Conclusions -----  TODO: add explanation \n")
-
-    # 3 ----------------------------------------------------------
-    # add gaussian noise - low
-    lena_gaussian = addGaussianNoise() # TODO - add low noise
-
-	# add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
-    plt.figure()
-    plt.subplot(2, 3, 1)
-    plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
-    plt.title("original")
-    plt.subplot(2, 3, 2)
-    plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
-    plt.title("gaussian noise - low")
-    plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    plt.title("median")
-    plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    plt.title("mean")
-    plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
-    plt.title("bilateral")
-
-    print("Conclusions -----  TODO: add explanation \n")
-
-    # 4 ----------------------------------------------------------
-    # add gaussian noise - high
-    lena_gaussian = addGaussianNoise() # TODO - add high noise
-	
-	# add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
-    plt.figure()
-    plt.subplot(2, 3, 1)
-    plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
-    plt.title("original")
-    plt.subplot(2, 3, 2)
-    plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
-    plt.title("gaussian noise - high")
-    plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    plt.title("median")
-    plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    plt.title("mean")
-    plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
-    plt.title("bilateral")
+    # plt.subplot(2, 3, 4)
+    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("median")
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("mean")
+    # plt.subplot(2, 3, 6)
+    # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("bilateral")
+    #
+    # print("Conclusions -----  TODO: add explanation\n")
+    #
+    # # 2 ----------------------------------------------------------
+    # # add salt and pepper noise - high
+    # lena_sp_high = addSPnoise() # TODO - add low noise
+    #
+    # # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
+    # plt.figure()
+    # plt.subplot(2, 3, 1)
+    # plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
+    # plt.title("original")
+    # plt.subplot(2, 3, 2)
+    # plt.imshow(lena_sp_high, cmap='gray', vmin=0, vmax=255)
+    # plt.title("salt and pepper - low")
+    # plt.subplot(2, 3, 4)
+    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("median")
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("mean")
+    # plt.subplot(2, 3, 6)
+    # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("bilateral")
+    #
+    # print("Conclusions -----  TODO: add explanation \n")
+    #
+    # # 3 ----------------------------------------------------------
+    # # add gaussian noise - low
+    # lena_gaussian = addGaussianNoise() # TODO - add low noise
+    #
+    # # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
+    # plt.figure()
+    # plt.subplot(2, 3, 1)
+    # plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
+    # plt.title("original")
+    # plt.subplot(2, 3, 2)
+    # plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
+    # plt.title("gaussian noise - low")
+    # plt.subplot(2, 3, 4)
+    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("median")
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("mean")
+    # plt.subplot(2, 3, 6)
+    # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("bilateral")
+    #
+    # print("Conclusions -----  TODO: add explanation \n")
+    #
+    # # 4 ----------------------------------------------------------
+    # # add gaussian noise - high
+    # lena_gaussian = addGaussianNoise() # TODO - add high noise
+    #
+    # # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
+    # plt.figure()
+    # plt.subplot(2, 3, 1)
+    # plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
+    # plt.title("original")
+    # plt.subplot(2, 3, 2)
+    # plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
+    # plt.title("gaussian noise - high")
+    # plt.subplot(2, 3, 4)
+    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("median")
+    # plt.subplot(2, 3, 5)
+    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("mean")
+    # plt.subplot(2, 3, 6)
+    # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
+    # plt.title("bilateral")
 
     print("Conclusions -----  TODO: add explanation \n")
 
