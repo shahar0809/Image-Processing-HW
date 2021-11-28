fc1024c0dd8b8eed86a6213c10ed495a69195291
Iris Berger <87372230+irisberger18@users.noreply.github.com>
2021-11-28 22:22:21 +0200

    Fourth script

--- HW3/hw3_functions.py
+++ HW3/hw3_functions.py
@@ -1,8 +1,9 @@
-import numpy as np
-import cv2
+import math
 import random
-import matplotlib.pyplot as plt
+
+import numpy as np
 from scipy.signal import convolve2d
+
 
 def addSPnoise(im, p):
     row, col = im.shape
@@ -10,9 +11,9 @@ def addSPnoise(im, p):
     num_noise_pixels = p * row * col
 
     # Generate random indexes for white and black pixels
-    white_pixels_x = random.sample(range(row * col), int(num_noise_pixels / 2))
-    black_pixels_x = random.sample(range(row * col), int(num_noise_pixels / 2))
-
+    pixels_index_list = set(range(row * col))
+    white_pixels_x = random.sample(pixels_index_list, int(num_noise_pixels / 2))
+    black_pixels_x = random.sample(pixels_index_list - set(white_pixels_x), int(num_noise_pixels / 2))
     # Assign white and black values
     sp_noise_im.ravel()[white_pixels_x] = 255
     sp_noise_im.ravel()[black_pixels_x] = 0
@@ -25,14 +26,27 @@ def addGaussianNoise(im, s):
     noisy_image = np.add(noisy_image, np.random.normal(0, s, im.shape))
     return noisy_image
 
+
 def cleanImageMedian(im, radius):
     median_im = im.copy()
-    # TODO: add implementation
+    for row in range(radius, im.shape[1] - radius):
+        for col in range(radius, im.shape[0] - radius):
+            filtering_mask = np.take(np.take(median_im, range(row - radius, row + radius + 1), 0),
+                                     range(col - radius, col + radius + 1), 1)
+            median_im[row][col] = np.median(filtering_mask)
     return median_im
 
 
 def cleanImageMean(im, radius, maskSTD):
-    # TODO: add implementation
+    col = list()
+    for x in range(-radius, radius):
+        row = list()
+        for y in range(-radius, radius):
+            row.append(math.exp(-((x ** 2 + y ** 2) / (2 * maskSTD ** 2))))
+        col.append(row)
+    mask = np.array(col)
+    gaussian_filter = mask / np.sum(mask)
+    cleaned_im = convolve2d(im, gaussian_filter, mode="same")
     return cleaned_im
 
 
--- HW3/hw3_script.py
+++ HW3/hw3_script.py
@@ -1,3 +1,6 @@
+import cv2
+import matplotlib.pyplot as plt
+
 from hw3_functions import *
 
 if __name__ == "__main__":
@@ -7,7 +10,7 @@ if __name__ == "__main__":
 
     # 1 ----------------------------------------------------------
     # add salt and pepper noise - low
-    lena_sp_low = addSPnoise(lena_gray, 0.2)
+    lena_sp_low = addSPnoise(lena_gray, 0.1)
 
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
@@ -17,21 +20,21 @@ if __name__ == "__main__":
     plt.subplot(2, 3, 2)
     plt.imshow(lena_sp_low, cmap='gray', vmin=0, vmax=255)
     plt.title("salt and pepper - low")
-    # plt.subplot(2, 3, 4)
-    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("median")
-    # plt.subplot(2, 3, 5)
-    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("mean")
+    plt.subplot(2, 3, 4)
+    plt.imshow(cleanImageMedian(lena_sp_low, 1), cmap='gray', vmin=0, vmax=255)
+    plt.title("median")
+    plt.subplot(2, 3, 5)
+    plt.imshow(cleanImageMean(lena_sp_low, 2, 8), cmap='gray', vmin=0, vmax=255)
+    plt.title("mean")
     # plt.subplot(2, 3, 6)
     # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
     # plt.title("bilateral")
-    #
-    # print("Conclusions -----  TODO: add explanation\n")
-    #
-    # # 2 ----------------------------------------------------------
+
+    print("Conclusions -----  TODO: add explanation\n")
+
+    # 2 ----------------------------------------------------------
     # add salt and pepper noise - high
-    lena_sp_high = addSPnoise(lena_gray, 0.8)
+    lena_sp_high = addSPnoise(lena_gray, 0.3)
 
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
@@ -41,21 +44,21 @@ if __name__ == "__main__":
     plt.subplot(2, 3, 2)
     plt.imshow(lena_sp_high, cmap='gray', vmin=0, vmax=255)
     plt.title("salt and pepper - high")
-    # plt.subplot(2, 3, 4)
-    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("median")
-    # plt.subplot(2, 3, 5)
-    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("mean")
+    plt.subplot(2, 3, 4)
+    plt.imshow(cleanImageMedian(lena_sp_high, 1), cmap='gray', vmin=0, vmax=255)
+    plt.title("median")
+    plt.subplot(2, 3, 5)
+    plt.imshow(cleanImageMean(lena_sp_high, 2, 8), cmap='gray', vmin=0, vmax=255)
+    plt.title("mean")
     # plt.subplot(2, 3, 6)
     # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
     # plt.title("bilateral")
-    #
-    # print("Conclusions -----  TODO: add explanation \n")
-    #
-    # # 3 ----------------------------------------------------------
-    # # add gaussian noise - low
-    lena_gaussian = addGaussianNoise(lena_gray, 10)
+
+    print("Conclusions -----  TODO: add explanation \n")
+
+    # 3 ----------------------------------------------------------
+    # add gaussian noise - low
+    lena_gaussian = addGaussianNoise(lena_gray, 20)
 
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
@@ -65,21 +68,21 @@ if __name__ == "__main__":
     plt.subplot(2, 3, 2)
     plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
     plt.title("gaussian noise - low")
-    # plt.subplot(2, 3, 4)
-    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("median")
-    # plt.subplot(2, 3, 5)
-    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("mean")
+    plt.subplot(2, 3, 4)
+    plt.imshow(cleanImageMedian(lena_gaussian, 1), cmap='gray', vmin=0, vmax=255)
+    plt.title("median")
+    plt.subplot(2, 3, 5)
+    plt.imshow(cleanImageMean(lena_gaussian, 2, 20), cmap='gray', vmin=0, vmax=255)
+    plt.title("mean")
     # plt.subplot(2, 3, 6)
     # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
     # plt.title("bilateral")
-    #
-    # print("Conclusions -----  TODO: add explanation \n")
-    #
-    # # 4 ----------------------------------------------------------
-    # # add gaussian noise - high
-    lena_gaussian = addGaussianNoise(lena_gray, 100)
+
+    print("Conclusions -----  TODO: add explanation \n")
+
+    # 4 ----------------------------------------------------------
+    # add gaussian noise - high
+    lena_gaussian = addGaussianNoise(lena_gray, 70)
 
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
@@ -89,12 +92,12 @@ if __name__ == "__main__":
     plt.subplot(2, 3, 2)
     plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
     plt.title("gaussian noise - high")
-    # plt.subplot(2, 3, 4)
-    # plt.imshow(cleanImageMedian(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("median")
-    # plt.subplot(2, 3, 5)
-    # plt.imshow(cleanImageMean(), cmap='gray', vmin=0, vmax=255)
-    # plt.title("mean")
+    plt.subplot(2, 3, 4)
+    plt.imshow(cleanImageMedian(lena_gaussian, 1), cmap='gray', vmin=0, vmax=255)
+    plt.title("median")
+    plt.subplot(2, 3, 5)
+    plt.imshow(cleanImageMean(lena_gaussian, 2, 70), cmap='gray', vmin=0, vmax=255)
+    plt.title("mean")
     # plt.subplot(2, 3, 6)
     # plt.imshow(bilateralFilt(), cmap='gray', vmin=0, vmax=255)
     # plt.title("bilateral")
