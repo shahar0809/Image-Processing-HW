fa241efd09197aa07332265d44992036954892ba
Iris Berger <87372230+irisberger18@users.noreply.github.com>
2021-12-05 19:58:41 +0200

    little change

--- HW3/hw3_functions.py
+++ HW3/hw3_functions.py
@@ -31,33 +31,38 @@ def cleanImageMedian(im, radius):
     median_im = im.copy()
     for row in range(radius, im.shape[0] - radius):
         for col in range(radius, im.shape[1] - radius):
-            filtering_mask = np.take(np.take(median_im, range(row - radius, row + radius + 1), 0),
-                                     range(col - radius, col + radius + 1), 1)
+            filtering_mask = median_im[row - radius:row + radius + 1, col - radius:col + radius + 1]
             median_im[row][col] = np.median(filtering_mask)
     return median_im
 
 
 def cleanImageMean(im, radius, maskSTD):
-    col = list()
-    for x in range(-radius, radius):
-        row = list()
-        for y in range(-radius, radius):
-            row.append(math.exp(-((x ** 2 + y ** 2) / (2 * maskSTD ** 2))))
-        col.append(row)
-    mask = np.array(col)
+    mask = np.zeros((2 * radius, 2 * radius))
+    for x in range(-radius, radius + 1):
+        for y in range(-radius, radius + 1):
+            mask[x][y] = math.exp(-((x ** 2 + y ** 2) / (2 * maskSTD ** 2)))
     gaussian_filter = mask / np.sum(mask)
     cleaned_im = convolve2d(im, gaussian_filter, mode="same")
     return cleaned_im
 
-
 def bilateralFilt(im, radius, stdSpatial, stdIntensity):
     bilateral_im = im.copy()
-    # for i in range(radius, im.shape[0] - radius):
-    #     for j in range(radius, im.shape[1] - radius):
-    #         # xx, yy = np.meshgrid(list(range(i - radius, i + radius + 1)), list(range(j - radius, j + radius + 1)))
-    #         # xy = np.vstack([xx.ravel(), yy.ravel()])
-    #         window = np.take(np.take(im, range(i - radius, i + radius + 1), 0), range(j - radius, j + radius + 1), 1)
-    #         gi = math.exp(-((math.pow(window- im[i][j], 2)) / (2 * stdIntensity ** 2)))
-    #         gs = math.exp(-((math.pow(xx - i, 2) + math.pow(yy - j, 2)) / (2 * stdSpatial ** 2)))
-    #         bilateral_im[i][j] = np.sum(np.multiply(gi, gs, window)) / np.sum(np.multiply(gi, gs))
+    for x in range(radius, im.shape[0] - radius):
+        for y in range(radius, im.shape[1] - radius):
+            window = bilateral_im[x - radius:x + radius + 1, y - radius:y + radius + 1]
+            xx, yy = np.meshgrid(list(range(x - radius, x + radius + 1)), list(range(y - radius, y + radius + 1)))
+
+            gs = np.exp(-((np.float_power(
+                window.astype(float) - (np.full(window.shape, im[x][y])).astype(float), 2)) / (
+                                  2 * stdIntensity ** 2)))
+            gs = gs / np.sum(gs)
+
+            gi = (np.exp(-((np.float_power(xx.ravel() - np.full(xx.ravel().shape, x), 2) + np.float_power(
+                yy.ravel() - np.full(yy.ravel().shape, y), 2)) / (2 * stdSpatial ** 2)))).reshape(gs.shape)
+            gi = gi.reshape(gs.shape) / np.sum(gi)
+
+            bilateral_im[x][y] = np.sum(np.matmul(gi.astype(float), np.matmul(gs.astype(float), window))) / np.sum(
+                np.matmul(gi.astype(float), gs.astype(float)))
+
+    print(np.sum(im), np.sum(bilateral_im))
     return bilateral_im
--- HW3/hw3_script.py
+++ HW3/hw3_script.py
@@ -5,12 +5,17 @@ from hw3_functions import *
 
 if __name__ == "__main__":
     # feel free to load different image than lena
-    lena = cv2.imread(r"Images\lena.tif")
+    lena = cv2.imread(r"Images\lena.jpg")
     lena_gray = cv2.cvtColor(lena, cv2.COLOR_BGR2GRAY)
 
     # 1 ----------------------------------------------------------
     # add salt and pepper noise - low
     lena_sp_low = addSPnoise(lena_gray, 0.1)
+
+    cv2.imwrite("add_low_noise_1_image.jpg", lena_sp_low)
+    cv2.imwrite("clean_median_1_image.jpg", cleanImageMedian(lena_sp_low, 1))
+    cv2.imwrite("clean_mean_1_image.jpg", cleanImageMean(lena_sp_low, 2, 8))
+    cv2.imwrite("clean_bilateral_1_image.jpg", bilateralFilt(lena_sp_low, 1, 8, 10))
 
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
@@ -18,16 +23,18 @@ if __name__ == "__main__":
     plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
     plt.title("original")
     plt.subplot(2, 3, 2)
-    plt.imshow(lena_sp_low, cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"add_low_noise_1_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("salt and pepper - low")
     plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(lena_sp_low, 1), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_median_1_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("median")
     plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(lena_sp_low, 2, 8), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_mean_1_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("mean")
     plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(lena_sp_low, 1, 8, 10), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_bilateral_1_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("bilateral")
 
     print("Conclusions -----  TODO: add explanation\n")
@@ -36,22 +43,29 @@ if __name__ == "__main__":
     # add salt and pepper noise - high
     lena_sp_high = addSPnoise(lena_gray, 0.3)
 
+    cv2.imwrite("add_high_noise_2_image.jpg", lena_sp_high)
+    cv2.imwrite("clean_median_2_image.jpg", cleanImageMedian(lena_sp_high, 1))
+    cv2.imwrite("clean_mean_2_image.jpg", cleanImageMean(lena_sp_high, 2, 8))
+    cv2.imwrite("clean_bilateral_2_image.jpg", bilateralFilt(lena_sp_high, 1, 8, 10))
+
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
     plt.subplot(2, 3, 1)
     plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
     plt.title("original")
     plt.subplot(2, 3, 2)
-    plt.imshow(lena_sp_high, cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"add_high_noise_2_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("salt and pepper - high")
     plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(lena_sp_high, 1), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_median_2_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("median")
     plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(lena_sp_high, 2, 8), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_mean_2_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("mean")
     plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(lena_sp_high, 1, 8, 10), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_bilateral_2_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("bilateral")
 
     print("Conclusions -----  TODO: add explanation \n")
@@ -60,22 +74,29 @@ if __name__ == "__main__":
     # add gaussian noise - low
     lena_gaussian = addGaussianNoise(lena_gray, 20)
 
+    cv2.imwrite("add_low_noise_3_image.jpg", lena_gaussian)
+    cv2.imwrite("clean_median_3_image.jpg", cleanImageMedian(lena_gaussian, 1))
+    cv2.imwrite("clean_mean_3_image.jpg", cleanImageMean(lena_gaussian, 2, 20))
+    cv2.imwrite("clean_bilateral_3_image.jpg", bilateralFilt(lena_gaussian, 1, 150, 20))
+
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
     plt.subplot(2, 3, 1)
     plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
     plt.title("original")
     plt.subplot(2, 3, 2)
-    plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"add_low_noise_3_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("gaussian noise - low")
     plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(lena_gaussian, 1), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_median_3_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("median")
     plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(lena_gaussian, 2, 20), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_mean_3_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("mean")
     plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(lena_gaussian, 1, 8, 20), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_bilateral_3_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("bilateral")
 
     print("Conclusions -----  TODO: add explanation \n")
@@ -84,22 +105,29 @@ if __name__ == "__main__":
     # add gaussian noise - high
     lena_gaussian = addGaussianNoise(lena_gray, 70)
 
+    cv2.imwrite("add_low_noise_4_image.jpg", lena_gaussian)
+    cv2.imwrite("clean_median_4_image.jpg", cleanImageMedian(lena_gaussian, 1))
+    cv2.imwrite("clean_mean_4_image.jpg", cleanImageMean(lena_gaussian, 2, 70))
+    cv2.imwrite("clean_bilateral_4_image.jpg", bilateralFilt(lena_gaussian, 1, 150, 70))
+
     # add parameters to functions cleanImageMedian, cleanImageMean, bilateralFilt
     plt.figure()
     plt.subplot(2, 3, 1)
     plt.imshow(lena_gray, cmap='gray', vmin=0, vmax=255)
     plt.title("original")
     plt.subplot(2, 3, 2)
-    plt.imshow(lena_gaussian, cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"add_low_noise_4_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("gaussian noise - high")
     plt.subplot(2, 3, 4)
-    plt.imshow(cleanImageMedian(lena_gaussian, 1), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_median_4_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("median")
     plt.subplot(2, 3, 5)
-    plt.imshow(cleanImageMean(lena_gaussian, 2, 70), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_mean_4_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0, vmax=255)
     plt.title("mean")
     plt.subplot(2, 3, 6)
-    plt.imshow(bilateralFilt(lena_gaussian, 1, 8, 70), cmap='gray', vmin=0, vmax=255)
+    plt.imshow(cv2.cvtColor(cv2.imread(r"clean_bilateral_4_image.jpg"), cv2.COLOR_BGR2GRAY), cmap='gray', vmin=0,
+               vmax=255)
     plt.title("bilateral")
 
     print("Conclusions -----  TODO: add explanation \n")
