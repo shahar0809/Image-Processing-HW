455c610cbfbf7033658073fc72fc858b732cb2d0
Iris Berger <87372230+irisberger18@users.noreply.github.com>
2022-01-05 21:08:56 +0200

    section 4 finished + trys of section 2, 3 and 5

--- HW5/hw5_functions.py
+++ HW5/hw5_functions.py
@@ -1,6 +1,9 @@
 import cv2.cv2 as cv2
-from scipy.signal import convolve2d
 import numpy as np
+from scipy.signal import convolve2d
+
+BlACK = (0, 255, 0)
+
 
 def sobel_edge_detection(img):
     sobel_vertical_kernel = np.array([
@@ -17,3 +20,41 @@ def sobel_edge_detection(img):
 
     vertical_edges = convolve2d(img, sobel_vertical_kernel)
     return convolve2d(vertical_edges, sobel_horizontal_kernel)
+
+
+def canny_edge_detection(image, threshold1, threshold2, aperture_size=3, l2_gradient=False):
+    return cv2.Canny(image, threshold1, threshold2, apertureSize=aperture_size, L2gradient=l2_gradient)
+
+
+def hugh_transform_circles(image):
+    blur_image = cv2.medianBlur(image, 9)
+    circles = np.uint16(np.around(cv2.HoughCircles(blur_image, cv2.HOUGH_GRADIENT, 1, 15,
+                                                   param1=59, param2=75, minRadius=2, maxRadius=0)))
+
+    for circle in circles[0]:
+        cv2.circle(image, (circle[0], circle[1]), circle[2], BlACK, 2)
+
+    return image
+
+
+def hugh_transform_lines(image):
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=250)
+    # Draw lines on the image
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(image, (x1, y1), (x2, y2), BlACK, 3)
+    # print(lines)
+    # for line in lines[0]:
+    #     a = np.cos(line[1])
+    #     b = np.sin(line[1])
+    #     x0 = a * line[0]
+    #     y0 = b * line[0]
+    #     x1 = int(x0 + 1000 * (-b))
+    #     y1 = int(y0 + 1000 * (a))
+    #     x2 = int(x0 - 1000 * (-b))
+    #     y2 = int(y0 - 1000 * (a))
+    #
+    #     cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    return image
--- HW5/hw5_script.py
+++ HW5/hw5_script.py
@@ -1,9 +1,10 @@
-from hw5_functions import *
 import matplotlib.pyplot as plt
+
+from hw5_functions import *
 
 
 def main_script():
-    # Applying Sobel Edge Detection on balls image
+    # section 1 - Applying Sobel Edge Detection on balls image
     im1 = cv2.imread("balls1.tif")
     im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
 
@@ -15,6 +16,79 @@ def main_script():
     plt.title("Sobel")
     im1_edge_det = sobel_edge_detection(im1)
     plt.imshow(im1_edge_det, cmap='gray', vmin=0, vmax=255)
+
+    # section 2
+    im2 = cv2.cvtColor(cv2.imread("coins1.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im2, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Canny")
+    im2_edge_detection = canny_edge_detection(im2, 150, 500, l2_gradient=True)
+    plt.imshow(im2_edge_detection, cmap='gray', vmin=0, vmax=255)
+
+    # section 3
+    im3 = cv2.cvtColor(cv2.imread("balls2.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im3, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Canny")
+    im3_edge_detection = canny_edge_detection(im3, 90, 120, l2_gradient=True)
+    plt.imshow(im3_edge_detection, cmap='gray', vmin=0, vmax=255)
+
+    # section 4 - Applying Hough Transform for circles on coins image
+    im4 = cv2.cvtColor(cv2.imread("coins3.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im4, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Hough Transform - Circles")
+    im4_hough_transform = hugh_transform_circles(im4)
+    plt.imshow(im4_hough_transform, cmap='gray', vmin=0, vmax=255)
+
+    # section 5 - Applying Hough Transform for lines on boxOfChocolates images
+    # boxOfChocolates1
+    im5 = cv2.cvtColor(cv2.imread("boxOfchocolates1.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im5, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Hough Transform - Lines, boxOfChocolates1")
+    im5_hough_transform = hugh_transform_lines(im5)
+    plt.imshow(im5_hough_transform, cmap='gray', vmin=0, vmax=255)
+
+    # boxOfChocolates2
+    im5 = cv2.cvtColor(cv2.imread("boxOfchocolates2.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im5, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Hough Transform - Lines, boxOfChocolates2")
+    im5_hough_transform = hugh_transform_lines(im5)
+    plt.imshow(im5_hough_transform, cmap='gray', vmin=0, vmax=255)
+
+    # boxOfChocolates2rot
+    im5 = cv2.cvtColor(cv2.imread("boxOfchocolates2rot.tif"), cv2.COLOR_BGR2GRAY)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.title("Original")
+    plt.imshow(im5, cmap='gray', vmin=0, vmax=255)
+    plt.subplot(1, 2, 2)
+    plt.title("Hough Transform - Lines, boxOfChocolates2rot")
+    im5_hough_transform = hugh_transform_lines(im5)
+    plt.imshow(im5_hough_transform, cmap='gray', vmin=0, vmax=255)
 
     plt.show()
 
