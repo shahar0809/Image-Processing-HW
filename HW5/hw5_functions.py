b63fadc8075b000814fe64ee69deb9028828c655
Shahar <shahartefler@gmail.com>
2022-01-08 13:19:00 +0200

    Did 5.1, add merging lines functions

--- HW5/hw5_functions.py
+++ HW5/hw5_functions.py
@@ -1,6 +1,7 @@
 import cv2.cv2 as cv2
 import numpy as np
 from scipy.signal import convolve2d
+import math
 
 BlACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -54,12 +55,9 @@ def hough_transform_circles(image):
 def hough_transform_lines1(image):
     image = cv2.medianBlur(image, 5)
     edges = cv2.Canny(image, 170, 296, apertureSize=3)
-    se = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    edges = cv2.dilate(edges, se, iterations=1)
-    edges = cv2.erode(edges, se, iterations=1)
-    lines = cv2.HoughLinesP(edges, 1, 0.3 * (np.pi / 180), 100, minLineLength=150, maxLineGap=13)
-    #lines = merge_lines(lines, 20)
-    return draw_lines(image, lines)
+    lines = cv2.HoughLines(edges, 2, 0.6 * (np.pi / 180), 250)
+    lines = merge_lines(lines, 0.5, 6)
+    return draw_lines_polar(image, lines)
 
 
 def hough_transform_lines2(image):
@@ -72,9 +70,31 @@ def hough_transform_lines3(image):
     return draw_lines(image, cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=10, maxLineGap=250))
 
 
+"""
+Utility functions for removing similar lines.
+"""
+
+def get_points(line):
+    r, theta = line
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * r
+    y0 = b * r
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    return np.array([x1, y1]), np.array([x2, y2])
+
+def get_line_equation(line):
+    point1, point2 = get_points(line)
+    slope = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    return slope, point1[1] - slope * point1[0]
+
+
 def dist_between_lines(line1, line2):
-    coefficients1 = np.polyfit(np.squeeze(line1)[:2], np.squeeze(line1)[2:], 1)
-    coefficients2 = np.polyfit(np.squeeze(line2)[:2], np.squeeze(line2)[2:], 1)
+    coefficients1 = get_line_equation(line1)
+    coefficients2 = get_line_equation(line2)
 
     slope1 = coefficients1[0]
     b1 = coefficients1[1]
@@ -84,20 +104,26 @@ def dist_between_lines(line1, line2):
 
     return abs(b1 - b2) / np.sqrt(np.square(slope1) + np.square(slope2))
 
-
-def merge_lines(lines, thresh):
+def merge_lines(lines, slope_thresh, b_thresh):
     merged_lines = np.squeeze(lines)
     for line1 in np.squeeze(lines):
         for line2 in np.squeeze(lines):
             if np.any(np.not_equal(line1, line2)):
-                if dist_between_lines(line1, line2) <= thresh:
+                l1 = get_line_equation(line1)
+                l2 = get_line_equation(line2)
+                if abs(l1[0] - l2[0]) < slope_thresh and dist_between_lines(line1, line2) < b_thresh:
                     merged_lines = merged_lines[np.not_equal(merged_lines, line2)[:, 0]]
     return list(merged_lines)
 
+def draw_lines_polar(image, lines):
+    for line in lines:
+        point1, point2 = get_points(line)
+        cv2.line(image, (point1[0], point1[1]), (point2[0], point2[1]), BlACK, 2)
+
+    return image
 
 def draw_lines(image, lines):
     for line in lines:
         x1, y1, x2, y2 = line[0]
         cv2.line(image, (x1, y1), (x2, y2), BlACK, 3)
-
     return image
